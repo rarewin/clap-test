54fba39d7f5ded7f2e44d241dbf2accb49b6d685
Katsuki Kobayashi <rare@tirasweel.org>
2019-03-21 00:18:56 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use clap::{App, Arg};
+
+fn main() {
+    let m = App::new("echo")
+        .arg(Arg::with_name("STRING").multiple(true))
+        .arg(
+            Arg::with_name("n")
+                .short("n")
+                .help("do not output the trailing newline"),
+        )
+        .get_matches();
+    let mut out = String::new();
+
+    if m.is_present("STRING") {
+        for v in m.values_of("STRING").unwrap() {
+            out.push_str(v);
+            out.push_str(" ");
+        }
+    }
+
+    out.pop(); // remove extra space
+
+    if !m.is_present("n") {
+        out.push_str("\n");
+    }
+
+    print!("{}", out);
+}
